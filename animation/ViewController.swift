065ddc8f06c4735f0031c5bbd5e338f4cad4c6c7
lighter <apple.xva@gmail.com>
2014-08-18 05:49:02 +0800

    新增red suqare view

--- animation/ViewController.swift
+++ animation/ViewController.swift
@@ -9,10 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+
+    let container = UIView()
+    let redSquare = UIView()
+    let blueSquare = UIView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // set container frame add to the screen
+        self.container.frame = CGRect(x: 60, y: 60, width: 200, height: 200)
+        self.view.addSubview(container)
+
+        // set red square frame up
+        // we want the blue square to have the same position as redSquare
+        // so lets just reuse blueSquare.frame
+        self.redSquare.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        self.blueSquare.frame = redSquare.frame
+
+        // set background colors
+        self.redSquare.backgroundColor = UIColor.redColor()
+        self.blueSquare.backgroundColor = UIColor.blueColor()
+
+        self.container.addSubview(self.redSquare)
+
     }
 
     override func didReceiveMemoryWarning() {
