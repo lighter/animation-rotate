8d834c82d3c52a94f190c3b6592d91dc918f8fdf
lighter <apple.xva@gmail.com>
2014-08-18 06:45:05 +0800

    Add animation function

--- animation/ViewController.swift
+++ animation/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     let redSquare = UIView()
     let blueSquare = UIView()
 
+    let button = UIButton.buttonWithType(UIButtonType.System) as UIButton
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -33,11 +35,60 @@ class ViewController: UIViewController {
 
         self.container.addSubview(self.redSquare)
 
+        // set button frame
+        self.setButton()
+        self.view.addSubview(self.button)
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    func setButton() {
+        let screenSize:CGRect = UIScreen.mainScreen().bounds
+        let screenWidth = screenSize.width
+        let screenHeight = screenSize.height
+        let buttonWidth = CGFloat(100)
+        let buttonHeight = CGFloat(25)
+        let doubleTwo = CGFloat(2)
+
+        let buttonX = (screenWidth / doubleTwo ) - (buttonWidth / doubleTwo)
+        let buttonY = screenHeight - (buttonHeight * doubleTwo)
+
+        self.button.frame = CGRect(x: buttonX, y: buttonY, width: buttonWidth, height: buttonHeight)
+        self.button.setTitle("Animation", forState: UIControlState.Normal)
+        self.button.addTarget(self, action: "animationClick:", forControlEvents:.TouchUpInside)
+    }
+
+    func animationClick(sender:UIButton!) {
+        print("click")
+
+        // create a 'tuple' (a pair of more of objects assigned to a sing)
+        var views = (frontView: self.redSquare, backView: self.blueSquare)
+
+        if(self.redSquare.isDescendantOfView(self.view)) {
+            views = (frontView:self.redSquare, backView: self.blueSquare)
+        }
+        else {
+            views = (frontView:self.blueSquare, backView: self.redSquare)
+        }
+
+        // set a transition style
+        let transitionOptions = UIViewAnimationOptions.TransitionCurlUp
+
+        UIView.transitionWithView(self.container, duration: 1.0, options: transitionOptions, animations: {
+            // remove the front object...
+            views.frontView.removeFromSuperview()
+
+            // ... and add the other object
+            self.container.addSubview(views.backView)
+
+            }, completion: { finished in
+                // any code entered here will be applied
+                // .once the animation has completed
+        })
     }
 
 
